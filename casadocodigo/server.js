5db55cfb79bba911217191c9a6b24ab609043f41
rayanepimentel <radharayg@gmail.com>
2019-12-14 19:25:45 -0300

    Concluindo a aula 01

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -3,14 +3,9 @@
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
-    //preciso verificar qual é o pedido que o usuário fez;
-    //pegando a requisição(req) e buscando a URL solicitada pelo usuário;
-    //se a url por / o que eu quero devolver é  a frase "Casa do código" (o html);
-    //antes vamos declarar, dentro da função callback, uma variável html que inicialmente não receberá valor algum.
 
     let html = '';
     if(req.url == '/'){
-        // se cairmos dentro desse if o html irá receber esse valor:
         html = `
             <html>
                 <head>
@@ -33,14 +28,8 @@ const servidor = http.createServer(function (req, resp) {
             </html>
         `;
     }
-    // e a resp vai ser com base na minha variável html
+    
     resp.end(html);
 })
 servidor.listen(3000);
 
-// eu quero criar outras routas e não só a routa raiz /
-// o que podemos fazer?
-//pode add else if ()?
-//vamos fazer um teste, cria um else para listar os livros
-//add else if() sucessivamente será que esse é o melhor caminho/abordagem?
-// Não!!!
